873fa40fb4be4c53c3f3a791cdbdfb824a2facc0
MatasKam <71984656+MatasKam@users.noreply.github.com>
2020-10-15 20:10:59 +0300

    Add files via upload

---
+++ biblioteka.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <random>
+#include <chrono>
+#include <numeric>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::rand;
+using std::setfill;
+using std::ifstream;
+using std::ofstream;
+using std::get;
+using std::sort;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+//using namespace std;
+
